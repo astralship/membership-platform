2f341bf02b82a267218cfba0e29f7cc4a5bf6c22
Michal Stefanow <mstefanow@gmail.com>
2017-04-16 12:07:45 +0100

    User page

--- js/profileCtrl.js
+++ js/profileCtrl.js
@@ -1,5 +1,7 @@
 app.controller("ProfileCtrl", function($rootScope, $scope) {
 
+	$scope.user = firebase.auth().currentUser;
+
 	firebase.database().ref('/events/').once('value').then(function(snapshot) {
 	  var data = snapshot.val();
 	  $scope.events = data;
--- js/userService.js
+++ js/userService.js
@@ -3,8 +3,12 @@ app.service("userService", function($rootScope) {
 	var service = {};
 
 	service.createUser = function(params) {
-
-
+		firebase.database().ref("users/" + params.uid).update(
+			{
+				"email": params.email,
+				"lastLogin": new Date().getTime()
+			}
+		);
 	};
 
 	return service;
