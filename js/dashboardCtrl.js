f1d7eb3eb486d711a0c6f09c79cd597bdaa468b7
Michal Stefanow <mstefanow@gmail.com>
2017-04-19 08:16:36 +0100

    Attending / Not attending

--- js/dashboardCtrl.js
+++ js/dashboardCtrl.js
@@ -1,22 +1,23 @@
-app.controller("DashboardCtrl", function($rootScope, $scope, eventService, userService) {
-
+app.controller("DashboardCtrl", function($rootScope, $scope, $q, eventService, userService) {
 
 	eventService.get().once('value').then(function(snapshot) {
 	  var data = snapshot.val();
 	  $scope.events = data;
+	  $scope.events.forEach(function(event) { event.attendees = event.attendees || {}; }) // sanite data so that I can add mark as attending later on
+	  console.log("Events: ", $scope.events);
 	  $scope.$apply();
 	});
 
-
-
-	$scope.attend = function(id) {
+	$scope.attend = function(id, event) {
+		event.attendees[$rootScope.user.uid] = true;
 		eventService.attend({
 			uid: $rootScope.user.uid,
 			eventId: id
 		})
 	};
 
-	$scope.cancel = function(id) {
+	$scope.cancel = function(id, event) {
+		event.attendees[$rootScope.user.uid] = false;
 		eventService.cancel({
 			uid: $rootScope.user.uid,
 			eventId: id
